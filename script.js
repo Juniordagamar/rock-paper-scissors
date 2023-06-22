43e30fe415b4e3fea5654549b23dd45a9700d942
Tsumbedzo Murulana <tsumbedzojnr@gmail.com>
2023-06-22 18:57:19 +0200

    Create getComputerChoice function

--- script.js
+++ script.js
@@ -5,5 +5,12 @@ let buttons = document.querySelectorAll("button");
 
 buttons.forEach(button => button.addEventListener("click", (event) => {
     let playerSelection = event.target.alt;
+    let computerSelection = getComputerChoice();
     console.log(playerSelection);
+    console.log(computerSelection)
 }))
+
+function getComputerChoice() {
+    let options = ["ROCK", "PAPER", "SCISSORS"];
+    return options[Math.floor(Math.random() * options.length)];
+}
