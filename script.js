fef2c2058ef5bb049c105d4ab8d7951e6cfdeeaf
Tsumbedzo Murulana <tsumbedzojnr@gmail.com>
2023-06-22 15:07:13 +0200

    Add another important variable

--- script.js
+++ script.js
@@ -4,6 +4,7 @@ let playerScore = 0;
 let computerScore = 0;
 
 const buttons = document.querySelectorAll(".button");
-const currentScore  = document.getElementsByClassName("player-score");
-const cpuScore = document.getElementsByClassName("cpu-score");
+const currentScore  = document.querySelector(".player-score");
+const cpuScore = document.querySelector(".cpu-score");
+const message = document.querySelector("h2");
 
