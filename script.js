00b3c289b36ce066f92bc5a9fbbd453ffa21bec5
Tsumbedzo Murulana <tsumbedzojnr@gmail.com>
2023-06-22 15:03:54 +0200

    Declare important variables

---
+++ script.js
@@ -0,0 +1,9 @@
+let playerSelection;
+let computerSelection;
+let playerScore = 0;
+let computerScore = 0;
+
+const buttons = document.querySelectorAll(".button");
+const currentScore  = document.getElementsByClassName("player-score");
+const cpuScore = document.getElementsByClassName("cpu-score");
+
