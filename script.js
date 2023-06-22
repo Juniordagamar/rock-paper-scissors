0fc5eb6b0ac4a3b3b8ed9e43e4f0f8633e07604c
Tsumbedzo Murulana <tsumbedzojnr@gmail.com>
2023-06-22 15:10:15 +0200

    Add click event to buttons

--- script.js
+++ script.js
@@ -8,3 +8,16 @@ const currentScore  = document.querySelector(".player-score");
 const cpuScore = document.querySelector(".cpu-score");
 const message = document.querySelector("h2");
 
+buttons.forEach((button) => {
+    button.addEventListener("click", () => {
+      const img = button.querySelector("img");
+      playerSelection = img.alt.toUpperCase();
+  
+      playRound(playerSelection, computerSelection);
+  
+      if (playerScore === 5 || computerScore === 5) {
+        declareWinner();
+      }
+    });
+  });
+  
