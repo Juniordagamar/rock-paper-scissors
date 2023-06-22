9ed6282dc23b227870da1899b6496f50b4fd6d32
Tsumbedzo Murulana <tsumbedzojnr@gmail.com>
2023-06-22 15:26:11 +0200

    Create getComputerChoice function

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ let computerSelection;
 let playerScore = 0;
 let computerScore = 0;
 
-const buttons = document.querySelectorAll(".button");
+let buttons = document.querySelectorAll(".button");
 const currentScore  = document.querySelector(".player-score");
 const cpuScore = document.querySelector(".cpu-score");
 const message = document.querySelector("h2");
@@ -11,8 +11,9 @@ const message = document.querySelector("h2");
 buttons.forEach((button) => {
     button.addEventListener("click", () => {
       const img = button.querySelector("img");
-      playerSelection = img.alt.toUpperCase();
-  
+      playerSelection = img.alt.toLowerCase();
+
+
       playRound(playerSelection, computerSelection);
   
       if (playerScore === 5 || computerScore === 5) {
@@ -20,4 +21,19 @@ buttons.forEach((button) => {
       }
     });
   });
+
+function getComputerChoice(computerChoice) {
+    let random = Math.floor(Math.random() * 3);
+
+    if (random == 0) {
+        computerChoice = "ROCK"
+    } else if (random == 1) {
+        computerChoice = "PAPER"
+    } else if (random == 2) {
+        computerChoice = "SCISSORS"
+    }
+
+    return computerChoice;
+}
+
   
